76c525c9e115756eac83d950e14cb3a4c22895da
cizquierdof <cizquierdof@gmail.com>
2020-08-03 18:55:32 +0200

    primera subida

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,63 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="../public/assets/logoFMC.webp" alt="">
+      </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link active">INICIO</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="servicios" class="nav-link">SERVICIOS</router-link> 
+        </li>
+        <li class="nav-item">
+          <router-link to="/nosotros" class="nav-link">NOSOTROS</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/contacto" class="nav-link">CONTACTO</router-link>
+        </li>
+        <!-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li> -->
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">BLOG</a>
+        </li>
+      </ul>
+      <!-- <form class="d-flex">
+        <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form> -->
     </div>
+  </div>
+</nav>
     <router-view/>
+    <Footer/>
   </div>
 </template>
+<script>
+import Footer from './components/Footer';
+export default {
+  components:{
+    Footer
+  }
+    
+}
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Contacto from '../views/Contacto.vue';
+import Servicios from '../views/Servicios.vue';
+import Nosotros from '../views/Nosotros.vue';
 
 Vue.use(VueRouter)
 
@@ -11,12 +14,19 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/contacto',
+    name: 'Contacto',
+    component: Contacto
+  },
+  {
+    path:'/servicios',
+    name: 'Servicios',
+    component: Servicios
+  },
+  {
+    path:'/nosotros',
+    name: 'Nosotros',
+    component: Nosotros
   }
 ]
 
